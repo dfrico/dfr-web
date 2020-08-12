eeff42626a0680ca5fd97e460faa4d785bfdbf92
Daniel Fernandez Rico <deferrico@gmail.com>
2020-08-12 18:45:26 +0200

    First build

---
+++ components/icon/component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Icon = ({ icon, className, style }) => (
+  <svg
+    className={`c-icon ${className}`}
+    viewBox={icon?.viewBox || '0 0 32 32'}
+    style={{ display: 'block', ...style }}
+  >
+    <use xlinkHref={`#${icon?.id || icon}`} />
+  </svg>
+);
+
+Icon.propTypes = {
+  icon: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.func,
+  ]),
+  className: PropTypes.string,
+  style: PropTypes.object,
+};
+
+Icon.defaultProps = {
+  className: '',
+};
+
+export default Icon;
--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,62 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+
+import cursor from "../services/cursor";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
+  useEffect(() => {
+    cursor();
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>dfr</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <div>
+          <header className={styles.header}>
+            <Link href="/">
+              <a>
+                <svg
+                  className={styles.logo}
+                  baseProfile="full"
+                  version="1.1"
+                  viewBox="0 0 930 540"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="m240 480a180 180 0 0 1 180 -180v0a180 180 0 0 1 180 180v0a180 180 0 0 1 180 -180m-405 180v0-360 360m90 0v0-180a135 180 0 0 1 135 -180m0 360v0-180"
+                    fill="none"
+                    stroke="#fff"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="42"
+                  />
+                </svg>
+              </a>
+            </Link>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+            <div className={styles.headerLinks}>
+              <Link href="/about">
+                <a className={styles.headerLink}>About </a>
+              </Link>
+              <Link href="/notes">
+                <a className={styles.headerLink}>Notes </a>
+              </Link>
+            </div>
+          </header>
+          <p>Daniel Fernandez</p>
+        </div>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+        <div id="cursor" className="cursor">
+          <div className="cursor__content"></div>
         </div>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    </>
+  );
 }
