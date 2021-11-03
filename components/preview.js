4f60fd219639bf269727ee1a5775950cb163e8f3
Daniel Fernandez Rico <daniel.fdez@pm.me>
2021-11-03 23:15:59 +0100

    Working with meta asynchronously to show previews client-side

--- components/preview.js
+++ components/preview.js
@@ -1,13 +1,21 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import Image from 'next/image';
 import Link from 'next/link';
 import PostHeader from 'components/postHeader';
 
 const PostPreview = ({ post }) => {
+  const [mounted, setMounted] = useState(false);
   const {
     link,
     module: { meta },
   } = post;
 
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!meta || !mounted) return null;
   return (
     <div className="rounded h-48 bg-gray-800 hover:bg-gray-700 transition-colors my-2">
       <Link href={`/blog${link}`}>
@@ -15,15 +23,13 @@ const PostPreview = ({ post }) => {
           <div className="w-2/3 p-4 flex flex-col justify-between flex-grow">
             <PostHeader meta={meta} />
           </div>
-          {meta.postImg ? (
+          {meta.postImg && (
             <div className="overflow-hidden rounded-r hidden sm:block h-full w-1/3 flex-grow-0">
-              <img
-                className="object-cover h-full"
-                src={meta.postImg}
-                alt={meta.title}
-              />
+              <div className="object-cover h-full relative">
+                <Image src={meta.postImg.src} alt={meta.title} layout="fill" />
+              </div>
             </div>
-          ) : null}
+          )}
         </a>
       </Link>
     </div>
