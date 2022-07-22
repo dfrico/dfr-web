ec5129e52ea5430275abeb66c89329e1f24dcd92
Daniel Fernandez Rico <daniel.fdez@pm.me>
2022-07-22 14:07:03 +0200

    2022 timeline update

--- components/timeline/timeline.js
+++ components/timeline/timeline.js
@@ -1,147 +1,156 @@
 import React from 'react';
 
-export default {
+/**
+ * Avoid using next/link to open links in a new tab.
+ */
+
+const timeline = {
+  2022: [
+    <>
+      Joined{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://fatmap.com">
+        Fatmap
+      </a>{' '}
+      in July 🏔.
+    </>,
+  ],
   2021: [
     <>
-      Just joined
-      {' '}
+      I joined{' '}
       <a
-        href="https://localistico.com"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://localistico.com"
       >
         Localistico
-      </a>
-      {' '}
-      in March! Stay tuned :)
+      </a>{' '}
+      in March. I was responsible for the maintenance and development of{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://localistico.com/location-and-presence-management/"
+      >
+        the main platform
+      </a>{' '}
+      written in Ember.js, and a couple of extra tools in React.
     </>,
   ],
   2020: [
     <>
-      Led the development of
-      {' '}
+      Led the development of{' '}
       <a
-        href="https://usa-resilience-opportunity-map.vercel.app/"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://usa-resilience-opportunity-map.vercel.app/"
       >
         USA Resilience Opportunity Map
       </a>
-      , and contributed to other big projects such as
-      {' '}
+      , and contributed to other big projects such as{' '}
       <a
-        href="https://opentimberportal.org"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://opentimberportal.org"
       >
         Open Timber Portal
       </a>
-      . All
-      {' '}
+      . All{' '}
       <a
-        href="https://github.com/vizzuality"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://github.com/vizzuality"
       >
         Open Source
       </a>
       !
     </>,
     <>
-      Led the development of
-      {' '}
+      Led the development of{' '}
       <a
-        href="http://www.globalforestwatch.org"
         target="_blank"
         rel="noopener noreferrer"
+        href="http://www.globalforestwatch.org"
       >
         Global Forest Watch
-      </a>
-      {' '}
+      </a>{' '}
       for a few months.
     </>,
     <>
-      I was going to give
-      {' '}
+      I was going to give{' '}
       <a
-        href="https://twitter.com/blayhem/status/1217729915954651136"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://twitter.com/blayhem/status/1217729915954651136"
       >
         a talk
-      </a>
-      {' '}
-      on this year’s
-      {' '}
-      <a href="https://t3chfest.es" target="_blank" rel="noopener noreferrer">
+      </a>{' '}
+      on this year’s{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://t3chfest.es">
         T3chfest
+      </a>{' '}
+      edition about{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://twitter.com/search?q=from%3A%40blayhem%20%23Inktober2019&src=typed_query&f=top"
+      >
+        creative coding/generative design
       </a>
-      {' '}
-      edition! Then COVID happened :(
+      , then COVID happened :(
     </>,
   ],
   2019: [
     <>
-      Bootstrapped projects such as
-      {' '}
+      Bootstrapped projects such as{' '}
       <a
-        href="https://thermal-assessment.climate.copernicus.eu/"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://thermal-assessment.climate.copernicus.eu/"
       >
         Copernicus Climate
-      </a>
-      {' '}
-      or
-      {' '}
+      </a>{' '}
+      or{' '}
       <a
-        href="https://forest-forward.com"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://forest-forward.com"
       >
         Forest Forward
       </a>
       .
     </>,
     <>
-      Created and maintained
-      {' '}
+      Created and maintained{' '}
       <a
-        href="https://github.com/Vizzuality/gfw/pull/3825"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://github.com/Vizzuality/gfw/pull/3825"
       >
         several data visualizations
-      </a>
-      {' '}
-      for
-      {' '}
+      </a>{' '}
+      for{' '}
       <a
-        href="https://www.globalforestwatch.org"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://www.globalforestwatch.org"
       >
         Global Forest Watch
       </a>
       , and contributed to the map.
     </>,
     <>
-      I won a
-      {' '}
+      I won a{' '}
       <a
-        href="https://twitter.com/blayhem/status/1117111829124341760"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://twitter.com/blayhem/status/1117111829124341760"
       >
         Facebook-adglow hackathon
-      </a>
-      {' '}
-      with an
-      {' '}
+      </a>{' '}
+      with an{' '}
       <a
-        href="https://twitter.com/JGFerreiro/status/1131424675828047873"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://twitter.com/JGFerreiro/status/1131424675828047873"
       >
         amazing multidisciplinary team
       </a>
@@ -150,38 +159,46 @@ export default {
   ],
   2018: [
     <>
-      Started working at
-      {' '}
-      <a
-        href="https://www.vizzuality.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Vizzuality
-      </a>
-      {' '}
-      as a Frontend Engineer. On a daily basis I use Nextjs, React, Redux, SASS,
-      Jest/Cypress, and D3/Recharts.
-    </>,
-    <>
-      Spent half of the year working at
-      {' '}
-      <a href="https://nddn.design" target="_blank" rel="noopener noreferrer">
+      Spent half of the year working at{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://nddn.design">
         Everis / NTT data design
       </a>
       . Contributed to the redesign of the digital experience of Burger King. I
       did user interviews, UX (flows, personas, navigation), and UI. Mostly, I
       worked on the responsive version of the web.
     </>,
+    <>
+      Finished my CS degree (UC3M), doing a final project on a tool to monitor
+      and predict the air quality in Madrid and surrounding areas. Not live now,
+      but{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.youtube.com/watch?v=al3KhRuHyuc"
+      >
+        you can see it here
+      </a>
+      !
+    </>,
+    <>
+      After that, I started working at{' '}
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.vizzuality.com"
+      >
+        Vizzuality
+      </a>{' '}
+      as a Frontend Engineer.
+    </>,
   ],
   2017: [
     <>
-      Did another internship at
-      {' '}
+      Did another internship at{' '}
       <a
-        href="https://www.graphext.com/"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://www.graphext.com/"
       >
         Graphext
       </a>
@@ -191,12 +208,11 @@ export default {
   ],
   2016: [
     <>
-      First internship at
-      {' '}
+      First internship at{' '}
       <a
-        href="https://www.graphext.com/"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://www.graphext.com/"
       >
         Graphext
       </a>
@@ -204,25 +220,24 @@ export default {
       scrapping with the main product API.
     </>,
     <>
-      Two classmates and I were
-      {' '}
+      Two classmates and I were{' '}
       <a
-        href="https://www.facebook.com/hackupc/photos/a.326823167691748/326851221022276/?type=3"
         target="_blank"
         rel="noopener noreferrer"
+        href="https://www.facebook.com/hackupc/photos/a.326823167691748/326851221022276/?type=3"
       >
         finalist at the HackUPC hackathon
-      </a>
-      {' '}
+      </a>{' '}
       in Barcelona.
     </>,
     <>
-      I was a volunteer organizer at
-      {' '}
-      <a href="https://t3chfest.es" target="_blank" rel="noopener noreferrer">
+      I was a volunteer organizer at{' '}
+      <a target="_blank" rel="noopener noreferrer" href="https://t3chfest.es">
         T3chfest
       </a>
       , a big tech-related event in Madrid.
     </>,
   ],
 };
+
+export default timeline;
