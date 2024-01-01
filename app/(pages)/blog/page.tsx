955bf67e8c279ae9fead06ce80ca6c125480e97d
Daniel Fernandez Rico <daniel.fdez@pm.me>
2024-01-01 23:12:21 +0100

    move all pages into app router directory

---
+++ app/(pages)/blog/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Preview from '@/components/preview';
+import postList from '@/utils/getAllPosts';
+
+export default function Blog() {
+  return (
+    <>
+      <br />
+      {postList.map((post) => (
+        <Preview key={post.link} post={post} />
+      ))}
+    </>
+  );
+}
