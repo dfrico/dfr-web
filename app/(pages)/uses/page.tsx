84172e9c308dd153caf754718215348b056629cb
Daniel Fernandez Rico <daniel.fdez@pm.me>
2024-01-02 00:31:45 +0100

    2023 /uses update

--- app/(pages)/uses/page.tsx
+++ app/(pages)/uses/page.tsx
@@ -9,7 +9,8 @@ export default function Uses() {
       <h1 className="font-bold font-rubik text-5xl tracking-tight mb-4 mt-16 text-yellow">
         /uses
       </h1>
-      <Image
+      {/* TODO: update pic */}
+      {/* <Image
         className="rounded-sm"
         src="/img/setup.jpg"
         alt="Setup"
@@ -18,60 +19,80 @@ export default function Uses() {
         style={{
           objectFit: 'contain',
         }}
-      />
+      /> */}
       <h2 className="mt-8">Working from home</h2>
       <ul className="mb-8">
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">
             2021 MacBook Pro with M1 Pro, 32GB of RAM and 1TB of SSD.
+          </span>
+        </li>
+        <li className="mb-2">
+          <span className="text-yellow">SFFPC</span> on a Ncase M1. It has an
+          i7-10700, 32GB of RAM, 500GB + 1TB of SSD, and a RTX 3070Ti. I use it
+          for gaming and some dev work.
+        </li>
+        <li className="mb-2">
+          <span className="text-yellow">Apple Studio Display</span> with
+          height-adjustable stand replacing my old{' '}
+          <span className="text-yellow">
+            Xiaomi 34&quot; ultrawide + Ergotron HX.
           </span>{' '}
-          Thicc boy, but the screen is totally worth it.
+          It connects to my SFFPC and macs via Thunderbolt 3, which allows me to
+          use the USB-C hub (+ethernet dongle), speakers, and webcam on both
+          computers just by switching the cable. The screen is absolutely great,
+          although I think it&apos;s a bit expensive considering it&apos;s a
+          60Hz panel and not OLED or miniLED.
         </li>
-        <li>
-          <span className="text-yellow">Xiaomi 34&quot; ultrawide.</span> Nice
-          resolution (1440p), good refresh rate (144Hz). The color profile is
-          not great with mac, and I will probably replace it soon, but for now
-          it does a pretty good job. It sits on an{' '}
-          <span className="text-yellow">Ergotron HX</span> VESA arm.
+        <li className="mb-2">
+          The speakers on the display replaced my old{' '}
+          <span className="text-yellow">
+            Yamaha HS7 studio monitors + Focusrite Scarlett Solo.
+          </span>{' '}
+          I still keep the Scarlett for music making, but I don&apos;t have it
+          on my desk.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">RAMA M65-b.</span> It&apos;s the KURO
           (黒) model, with Aliaz 60gr switches + GMK stabs (w/ krytox 205g0),
           and PBT Heavy Industry keycaps.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Logitech Ergo MX.</span> I tried a
           Glorious model O and an MX Master, and ended up using this one as my
-          daily driver, it&apos;s more comfortable. The mousepad is from Amazon,
-          90x40cm.
-        </li>
-        <li>
-          <span className="text-yellow">Yamaha HS5 studio monitors</span> with a{' '}
+          daily driver, it&apos;s more comfortable. For gaming, I have a{' '}
           <span className="text-yellow">
-            Focusrite Scarlett Solo audio interface
+            Logitech G Pro X Superlight Wireless.
           </span>
-          . They sound really good but they take a lot of space in my desk. The
-          Scarlett is fantastic for its price and size.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Airpods Max.</span> I used the Bose QC35
           for years, and recently switched to these ones. So far I&apos;m happy
-          with everything except for the <del>bra</del> case.
+          with everything except for the <del>bra</del> case. For gaming, I have
+          a <span className="text-yellow">Logitech G PRO X Wireless.</span>
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Fully Jarvis Desk.</span> Solid standing
           desk, I got the BYOD 3-stage frame and DIY-ed a tabletop with a beech
           kitchen countertop, 180x80cm (~70x31&quot;).
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Autonomous ErgoChair 2,</span> which is
           an older model similar to the current Pro one.
+        </li>
+        <li className="mb-2">
+          Other accesories are a{' '}
+          <span className="text-yellow">
+            Belkin 3-in-1 Wireless MagSafe Charger
+          </span>
+          , a <span className="text-yellow">Xiaomi Desk Lamp 1S</span>, and a{' '}
+          <span className="text-yellow">Chilly&apos;s 2 Flip bottle.</span>
         </li>
       </ul>
 
       <h2>Software and tools</h2>
       <ul className="mb-8">
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Visual Studio Code or VIM</span> are my
           main editors (my{' '}
           <a
@@ -83,31 +104,31 @@ export default function Uses() {
           </a>
           ).
         </li>
-        <li>
+        <li className="mb-2">
           I use <span className="text-yellow">Moonlight II italic</span> in
           VScode, and a customized gruvbox in VIM, with{' '}
           <span className="text-yellow">Dank Mono</span> font for everything
           (editor and terminal).
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">iTerm 2.</span> I tried Hyper but I had
           the feeling iTerm worked better on my machine.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Firefox</span> is my main browser for
           personal and dev use.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Fork</span> is a git GUI app that I like
           a lot. I use the git cli for simple stuff but for complex workflows
           this one is perfect.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Things</span> is my favorite reminders
           app, and <span className="text-yellow">Fantastical</span> is my go-to
           calendar app in macOS.
         </li>
-        <li>
+        <li className="mb-2">
           I use <span className="text-yellow">Figma</span> for personal design
           work, <span className="text-yellow">Darkroom</span> for photo editing,
           and <span className="text-yellow">Pixelmator Pro</span> for{' '}
@@ -117,36 +138,42 @@ export default function Uses() {
 
       <h2>On the go</h2>
       <ul className="mb-8">
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">ECD is an iPhone 13 Pro</span>, 6y.o.
           Nomatic wallet, keys etc.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Apple Watch Series 7</span> (41mm alu
           LTE). The always-on and slightly bigger display are great improvements
-          from my previous S4.
+          from my previous S4. I use either the{' '}
+          <span className="text-yellow">solo loop</span> or the{' '}
+          <span className="text-yellow">link bracelet.</span>
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Airpods Pro.</span> When I&apos;m
           traveling, I ditch the big headphones for smaller earbuds, but ANC is
           a must.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Apple Magsafe Duo.</span> Yeah I know.
           It&apos;s a great charger for my phone and watch and it fits very well
           in my <span className="text-yellow">Native Union stow organizer</span>
           , with the MBP charger and other cables.
         </li>
-        <li>
-          <span className="text-yellow">Kindle Paperwhite 2013.</span> Old but
-          reliable, I use it with Calibre on the mac.
+        <li className="mb-2">
+          <span className="text-yellow">2022 Kindle.</span> I loved my old 2013
+          paperwhite, but the new basic models come with USB-C, 300ppi, and
+          backlit —no need for the paperwhite model. I use it with Calibre.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Muji B6 or Midori A6 notebooks</span>,
           with a <span className="text-yellow">Lamy Safari F</span> or some{' '}
-          <span className="text-yellow">Muji .38 pens.</span>
+          <span className="text-yellow">Muji .38 pens.</span> I&apos;m also into
+          mechanical pencils, and I have a{' '}
+          <span className="text-yellow">Rotring 600</span> and a{' '}
+          <span className="text-yellow">Caran d&apos;Ache</span>.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Topo Rover</span> or{' '}
           <span className="text-yellow">Herschel Little America</span>{' '}
           backpacks, depends on the trip.
@@ -155,21 +182,22 @@ export default function Uses() {
 
       <h2>Music making</h2>
       <ul className="mb-12">
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Ableton Live and Ableton Push 2.</span>
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Native Instruments A49.</span> Not the
           best keyboard around and I don&apos;t love the software, but the size
-          and the keys are good. I used two keyboard sliders to mount it under
-          the desk.
+          and the keys are good. I&apos;ve been using my{' '}
+          <span className="text-yellow">Novation Launchkey Mini MK3</span> more
+          lately.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Volca Bass and Volca FM.</span> Little
           hardware synths, the Bass is fun to play with, the FM not so much but
           the sounds are good.
         </li>
-        <li>
+        <li className="mb-2">
           <span className="text-yellow">Schecter Omen 6.</span> I had this
           guitar for 10 years now, and I like it a lot.
         </li>
